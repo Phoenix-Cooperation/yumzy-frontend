c770cbef6e5b5891b0b4fcd80d6120494978f889
ravinduf <ravinduf@rocketmail.com>
2022-01-22 18:47:04 +0530

    created Schema.js and moved the two schemas, default values objects to it

---
+++ src/pages/Auth/Schema.js
@@ -0,0 +1,46 @@
+import * as yup from "yup";
+
+export const SignupSchema = yup.object().shape({
+    username: yup
+        .string()
+        .required("your name is required"),
+    email: yup
+        .string()
+        .email()
+        .required("your email is required"),
+    password: yup
+        .string()
+        .min(8, "password should have at least 8 characters")
+        .required("password is required"),
+    confirmPassword: yup
+        .string()
+        .required("confirm password is required")
+        .when("password", {
+            is: password => (password && password.length > 0 ? true : false),
+            then: yup.string().oneOf([yup.ref("password")], "Password doesn't match")
+        })
+});
+
+
+export const SigninSchema = yup.object().shape({
+    email: yup
+        .string()
+        .email()
+        .required("your email is required"),
+    password: yup
+        .string()
+        .min(8, "password should have at least 8 characters")
+        .required("password is required"),
+});
+
+export const signUpDefaultValues = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+}
+
+export const signInDefaultValues = {
+    email: "",
+    password: "",
+}

3b53ed879b0cb9f54dd3b8427a4298f3f753191b
ravinduf <ravinduf@rocketmail.com>
2022-03-13 21:55:02 +0530

    created isLoggedIn reactive var and made the local state management through the cache

--- src/api/cache.js
+++ src/api/cache.js
@@ -1,3 +1,17 @@
-import { InMemoryCache } from "@apollo/client";
+import { InMemoryCache, makeVar } from "@apollo/client";
 
-export const cache = new InMemoryCache({});
+export const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        isLoggedIn: {
+          read() {
+            return isLoggedIn();
+          }
+        }
+      }
+    }
+  }
+});
+
+export const isLoggedIn = makeVar(!!localStorage.getItem("token"))
--- src/utils/useAuth.js
+++ src/utils/useAuth.js
@@ -1,8 +1,16 @@
+import { gql, useQuery } from "@apollo/client";
+
 const useAuth = () => {
-  if (localStorage.getItem("token")) {
-    return true;
-  }
-  return false;
+
+  const IS_LOGGED_IN = gql`
+    query IsUserLoggedIn {
+      isLoggedIn @client
+    }
+  `;
+
+  const { data } = useQuery(IS_LOGGED_IN);
+  console.log(data.isLoggedIn, "Loggend Auth")
+  return !!data.isLoggedIn
 }
 
 export default useAuth;

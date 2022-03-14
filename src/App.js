4e4d84c2b96084acd9214f5e72623b18e79cf194
ravinduf <ravinduf@rocketmail.com>
2022-03-14 10:12:14 +0530

    created basic UserLayout, Navbar and added them to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./assets/stylesheets/main.scss";
+import UserLayout from "./Layouts/UserLayout";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/Home/Home";
 import { logout } from "./services/firebase-auth"
@@ -13,11 +14,12 @@ import PrivateRoute from "./utils/PrivateRoute";
 function App() {
   return (
     <div>
-      yumzy
       <BrowserRouter>
         <Routes>
           <Route path="/auth" element={<Auth />} />
-          <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/" element={<UserLayout/>} >
+            <Route path="home" element={<Home />} />
+          </Route>
         </Routes>
       </BrowserRouter>
       {/* <button onClick={logout}>Logout</button> */}

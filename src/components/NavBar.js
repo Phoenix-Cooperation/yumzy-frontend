af8ca3ab1629cd23daf2d9ff888e7f6b50acd83b
ravinduf <ravinduf@rocketmail.com>
2022-03-22 19:28:54 +0530

    added icons, styling for navabar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,49 @@
 
 import React from "react";
-import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, Container, Nav, Form, FormControl, Button } from "react-bootstrap";
+
+import { ReactComponent as Home} from "../assets/images/icons/home.svg"
+import { ReactComponent as Notifications} from "../assets/images/icons/bell.svg"
+import { ReactComponent as Chat} from "../assets/images/icons/chat.svg"
+import { ReactComponent as Explore} from "../assets/images/icons/explore.svg"
+import { ReactComponent as Logo} from "../assets/images/Logo.svg"
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Brand href="#">
+          <Logo className="navbar__logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-            Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-          Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+          <div className="navbar__search">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-primary">Search</Button>
+            </Form>
+            <Nav
+              className="navbar__links"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1"><Home className="navbar__linkImg"/></Nav.Link>
+              <Nav.Link href="#action2"><Chat className="navbar__linkImg"/></Nav.Link>
+              <Nav.Link href="#action2"><Notifications className="navbar__linkImg"/></Nav.Link>
+              <Nav.Link href="#action2"><Explore className="navbar__linkImg"/></Nav.Link>
+            </Nav>
+          </div>
         </Navbar.Collapse>
+
+        <div className="navbar__avatar">
+
+        </div>
+
       </Container>
     </Navbar>
   )

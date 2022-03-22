3c85c730ceb13648a133a1f080389a24a644ec16
ravinduf <ravinduf@rocketmail.com>
2022-03-22 19:33:15 +0530

    fixed logo styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,22 @@
 import React from "react";
 import { Navbar, Container, Nav, Form, FormControl, Button } from "react-bootstrap";
 
-import { ReactComponent as Home} from "../assets/images/icons/home.svg"
-import { ReactComponent as Notifications} from "../assets/images/icons/bell.svg"
-import { ReactComponent as Chat} from "../assets/images/icons/chat.svg"
-import { ReactComponent as Explore} from "../assets/images/icons/explore.svg"
-import { ReactComponent as Logo} from "../assets/images/Logo.svg"
+import { ReactComponent as Home } from "../assets/images/icons/home.svg"
+import { ReactComponent as Notifications } from "../assets/images/icons/bell.svg"
+import { ReactComponent as Chat } from "../assets/images/icons/chat.svg"
+import { ReactComponent as Explore } from "../assets/images/icons/explore.svg"
+import { ReactComponent as Logo } from "../assets/images/Logo.svg"
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">
-          <Logo className="navbar__logo"/>
-        </Navbar.Brand>
+        <div href="#">
+          <Nav.Link>
+            <Logo className="navbar__logo" />
+
+          </Nav.Link>
+        </div>
         <Navbar.Toggle />
         <Navbar.Collapse id="navbarScroll">
           <div className="navbar__search">
@@ -32,10 +35,10 @@ const NavBar = () => {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="#action1"><Home className="navbar__linkImg"/></Nav.Link>
-              <Nav.Link href="#action2"><Chat className="navbar__linkImg"/></Nav.Link>
-              <Nav.Link href="#action2"><Notifications className="navbar__linkImg"/></Nav.Link>
-              <Nav.Link href="#action2"><Explore className="navbar__linkImg"/></Nav.Link>
+              <Nav.Link href="#action1"><Home className="navbar__linkImg" /></Nav.Link>
+              <Nav.Link href="#action2"><Chat className="navbar__linkImg" /></Nav.Link>
+              <Nav.Link href="#action2"><Notifications className="navbar__linkImg" /></Nav.Link>
+              <Nav.Link href="#action2"><Explore className="navbar__linkImg" /></Nav.Link>
             </Nav>
           </div>
         </Navbar.Collapse>

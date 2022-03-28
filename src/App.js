ff8294f50288a6907f66f033f2fb55d6a6c9efa0
ravinduf <ravinduf@rocketmail.com>
2022-03-29 02:46:06 +0530

    createMenu basic features done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./assets/stylesheets/main.scss";
 import UserLayout from "./layouts/UserLayout";
+import CreateMenu from "./layouts/CreateMenu";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/Home/Home";
 import { logout } from "./services/firebase-auth"
@@ -17,6 +18,12 @@ function App() {
           <Route path="/auth" element={<Auth />} />
           <Route path="/" element={<UserLayout/>} >
             <Route path="home" element={<Home />} />
+            <Route path="create" element={<CreateMenu/>} >
+              <Route index element={<Home />} />
+              <Route path="recipe" element={<Home />} />
+              <Route path="tips" element={<Home />} />
+              <Route path="review" element={<Home />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>

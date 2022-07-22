12de4d5086428645793f03bc6d1e7c8f8e90f9a5
ravinduf <ravinduf@rocketmail.com>
2022-07-22 11:09:36 +0530

    added ImageUpload component and tags input

--- src/pages/createContent/PostPanel.js
+++ src/pages/createContent/PostPanel.js
@@ -4,6 +4,7 @@ import {CREATE_POST} from "../../Graphql/mutations/contentCreateMutation";
 import {ReactComponent as Xmark} from "../../assets/images/icons/x-mark.svg";
 import _ from "lodash";
 import Compress from "compress.js";
+import ImageUpload from "./ImageUpload";
 
 
 const PostPanel = () => {
@@ -13,47 +14,21 @@ const PostPanel = () => {
   const imageInput = useRef();
   const [description, setDescription] = useState("");
   const [imageObjects, setImageObjects] = useState([]);
+  const [uploadedImages, setUploadedImages] = useState([]);
+  const [tags, setTags] = useState([]);
 
   /**
    * error messages
    * */
-  const [errorImageUpload, setErrorImageUpload] = useState("");
+  const [errorImageUpload, setErrorImageUpload] = useState([]);
   const [createPost, {error}] = useMutation(CREATE_POST);
 
-  /**
-   * image validation and assign to variable
-   * */
-  const onImageChange = (event) => {
+  const handleErrorImageUpload = (error) => {
+    setErrorImageUpload((prev) => [...prev, error ])
+  }
 
-    if (event.target.files && event.target.files.length > 0) {
-      _.forEach(event.target.files, file => {
-
-        if (!file) {
-          setErrorImageUpload("Image is not valid");
-          return;
-        }
-
-        if (!file.name.match(/\.(jpg|jpeg|png)$/)) {
-          setErrorImageUpload("Image type is not valid");
-          return;
-        }
-
-        if (file.size > 10e6) {
-          setErrorImageUpload("Please upload a file smaller than 10 MB");
-          return;
-        }
-
-        // updates model
-        setImageObjects((prevState => [
-            ...prevState, {file: resizeImageFn(file), imageURL: URL.createObjectURL(file)}
-          ]
-        ));
-      });
-    }
-  };
-
-  const focusImageUploadInput = () => {
-    imageInput.current.click();
+  const handleImageObjectsChange = ({file, imageURL}) => {
+    setImageObjects((prev) => [...prev, {file, imageURL}])
   }
 
   /**
@@ -61,23 +36,6 @@ const PostPanel = () => {
    * */
   const removeFileArrayValue = (index) => {
     setImageObjects(imageObjects.filter(image => image !== imageObjects[index]));
-  }
-
-  async function resizeImageFn(file) {
-
-    const resizedImage = await compress.compress([file], {
-      size: 2, // the max size in MB, defaults to 2MB
-      quality: 1, // the quality of the image, max is 1,
-      maxWidth: 300, // the max width of the output image, defaults to 1920px
-      maxHeight: 300, // the max height of the output image, defaults to 1920px
-      resize: true // defaults to true, set false if you do not want to resize the image width and height
-    });
-
-    const img = resizedImage[0];
-    const base64str = img.data
-    const imgExt = img.ext
-    const resizedFile = Compress.convertBase64ToFile(base64str, imgExt)
-    return resizedFile;
   }
 
   /**
@@ -124,36 +82,42 @@ const PostPanel = () => {
           Images
         </label>
         <div className="createContent__form__imagesUploadMain">
-          <input
-            hidden
-            type="file"
-            name="image"
-            multiple
-            accept=".jpg, .jpeg, .png"
-            ref={imageInput}
-            onChange={onImageChange}/>
-          <div
-            className="createContent__form__imagesUploadMain__imagesInput"
-            onClick={focusImageUploadInput}>
-            UPLOAD
-          </div>
+          <ImageUpload
+            handleErrors={handleErrorImageUpload}
+            handleImageChange={handleImageObjectsChange}
+          />
           <div className="createContent__form__imagesUploadMain__imageView">
             {
               imageObjects.map((image, index) => (
-                  <div key={index + "image-view-parent"}>
-                    <img
-                      className="createContent__form__imagesUploadMain__imageView__image"
-                      key={index + "image"}
-                      src={image.imageURL}
-                      alt="..."/>
-                    <Xmark onClick={() => removeFileArrayValue(index)}/>
-                  </div>
-                )
+                <div key={index + "image-view-parent"}>
+                  <img
+                    className="createContent__form__imagesUploadMain__imageView__image"
+                    key={index + "image"}
+                    src={image.imageURL}
+                    alt="..."/>
+                  <Xmark onClick={() => removeFileArrayValue(index)}/>
+                </div>
               )
-            }
+              )}
           </div>
         </div>
-
+        
+        <label
+          className="createContent__form__tagsLabel"
+          htmlFor="tags">
+            Tags
+        </label>
+        <input
+          className="createContent__form__tagsInput"
+          type="text"
+          id="tags"
+          name="tags"
+          onChange={(event) => {
+            const { value } = event.target;
+            const tempTags = value.split(" ")
+            setTags(tempTags)
+          }}/>
+          
         <button
           className="createContent__form__submit"
           type="submit"

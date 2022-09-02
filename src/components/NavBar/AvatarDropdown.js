33fb55d4c927517b141885a315d574850350ba32
ravinduf <ravinduf@rocketmail.com>
2022-09-02 20:30:31 +0530

    styled and improved the AvatarDropdown

--- src/components/NavBar/AvatarDropdown.js
+++ src/components/NavBar/AvatarDropdown.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect, useRef } from "react"
+
 import { gql, useQuery } from "@apollo/client";
 import { CSSTransition } from "react-transition-group";
 
 import ProfilePic from "../../assets/images/icons/profile.svg"
+import DropdownContent from "./DropdownContent";
 
 const AvatarDropdown = () => {
 
   const [showDropDown, setShowDropDown] = useState(false);
   const [dropDownHeight, setDropDownHeight] = useState(null);
+  const [dropDownWidth, setDropDownWidth] = useState(null);
+  const [user, setUser] = useState(null);
   const dropdownRef = useRef(null);
 
   const GET_USER = gql`
@@ -20,11 +24,25 @@ const AvatarDropdown = () => {
     setDropDownHeight(0)
   }, [])
 
+
   const { data } = useQuery(GET_USER);
+  useEffect(() => {
+    setUser(data?.user)
+  })
   
   function calcHeight(el) {
-    const height = el.offsetHeight;
-    console.log(height)
+    console.log(el.offsetWidth * 0.2, "offset width")
+    console.log(el.offsetWidth, "offset width")
+    const height = el.offsetHeight + el.offsetHeight * 0.09;
+    const width = el.offsetWidth + el.offsetWidth * 0.2;
+
+    if (width > 180) {
+      setDropDownWidth(240);
+    } else {
+      setDropDownWidth(180)
+    }
+
+    
     setDropDownHeight(height);
   }
 
@@ -34,30 +52,26 @@ const AvatarDropdown = () => {
     setDropDownHeight(0)
   }
 
-  console.log(showDropDown);
+  
   return (
     <div className="avatarDropDown">
       <div className="avatarDropDown__avatar" onClick={handleShowDropDown}>
-        {data?.user && data.user.photoURL ? 
-          (<img src={data.user.photoURL} alt="dp" referrerPolicy="no-referrer"/>) :
+        {user && user.photoURL ? 
+          (<img src={user.photoURL} alt="dp" referrerPolicy="no-referrer"/>) :
           (<img src={ProfilePic} alt="dp" />)
         }
       </div>
 
-      <div className="avatarDropDown__dropDown" style={{ height: dropDownHeight, display: showDropDown ? "block" : "none" }} ref={dropdownRef}>
+      <div className="avatarDropDown__dropDown" style={{ height: dropDownHeight, display: showDropDown ? "block" : "none", width: dropDownWidth }} ref={dropdownRef}>
         <CSSTransition
           in={showDropDown}
-          timeout={500}
+          timeout={5000}
           classNames="avatarDropDown__dropDown__group"
           unmountOnExit
           onEnter={calcHeight}
           // onExited={calcHeight}
         >
-          <div>
-            <div>Settings</div>
-            <div>My Profile</div>
-            <div>Logout</div>
-          </div>
+          <DropdownContent user={user}/>
         </CSSTransition>
       </div>
     </div>

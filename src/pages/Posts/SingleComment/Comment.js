578e3a52fc92f3743fd7fc3af285a471d9b457a3
ravinduf <ravinduf@rocketmail.com>
2023-02-08 18:02:09 +0530

    improved

--- src/pages/Posts/SingleComment/Comment.js
+++ src/pages/Posts/SingleComment/Comment.js
@@ -1,21 +1,23 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import {Col, Row} from "react-bootstrap";
+import {Col} from "react-bootstrap";
 import { ReactComponent as User } from "../../../assets/images/icons/account.svg"
 
 const Comment = ({comment}) => {
 
   return (
     <div className="comment__box">
-      <Row className="comment__commentbox">
-        <Col md={1} className="comment__column">{comment.user.photoURL !== null || comment.user.photoURL !== "" || comment.user.photoURL !== undefined ?
-          (<img className="comment__avatar" src={comment.user.photoURL} alt="" />) :
-          (<User className="comment__avatar"/>)}</Col>
-        <Col md={8} className="comment__column"><span className="comment__userName">{comment.user.username}</span></Col>
-      </Row>
-      <Row>
+      <Col md={1} className="comment__avatarCol">
+        <div>
+          {comment.user.photoURL !== null || comment.user.photoURL !== "" || comment.user.photoURL !== undefined ?
+            (<img className="comment__avatar" src={comment.user.photoURL} alt="" />) :
+            (<User className="comment__avatar"/>)}
+        </div>
+      </Col>
+      <Col md={10} className="comment__commentCol">
+        <span className="comment__userName">{comment.user.username}</span>
         <span className="comment__comment">{comment.comment}</span>
-      </Row>
+      </Col>
     </div>
   );
 };
